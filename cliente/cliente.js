4cd9b1256727e61964a438e9ee10d0ae660cd2fb
Fernando Izquierdo Romera <fer227@correo.ugr.es>
2021-01-26 18:17:26 +0000

    ejercicio terminado

--- cliente/cliente.js
+++ cliente/cliente.js
@@ -1,5 +1,20 @@
-await fetch('http://0.0.0.0:6000/libros', {
-    method: 'GET'    
-}).then((resultado) => {
-    console.log(resultado);
-})
+const fetch = require("node-fetch");
+
+async function getData(){
+
+    // await new Promise(resolve => setTimeout(resolve, 1200));
+
+    return await fetch('http://localhost:6000/libros', {
+        method: 'GET'    
+    });
+}
+
+function handlerRequest(){
+    getData().then((response) => {
+        console.log(response.json().then((res) => {console.log(res)}));
+    }).catch((error) => {
+        console.log(error);
+    }) 
+}
+
+setTimeout(handlerRequest, 500);
